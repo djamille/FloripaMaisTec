96a9078af7f91085472dc98dad71e085892e5485
Djamille Vieira <djamille@hotmail.com>
2023-02-08 16:04:45 -0300

    AverageStudenta - Média do estudant. Código em C#

---
+++ modulo01/semana01/Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,15 @@
+﻿int n1 = 0, n2 = 1, n3 = 0;
+
+Console.Write("Digite qual será a quantidade de números calculados pela sequência de Fibonacci. \n\n");           //Solicitando ao usuário quantidade de numeros que deve ter a sequencia.
+int fib = int.Parse(Console.ReadLine());            //Entrada de dados, pelo usuario.
+
+int[] fibonacci = new int[fib];            //Criando o vetor com o tipo inteiro.
+
+Console.WriteLine("Sequência de Fibonacci com " + fib + " números: ");
+for (int i = 0; i < fib; i++)         //Enquanto a variavel 'i' for menor que a variavel 'fib', as informações digitadas pelo usuario serao gravadas de caixa em caixa no vetor.
+{
+    n3 = n1 + n2;         //Somatória 
+    Console.Write(n3 + " ");          //Saida dados, imprime na tela o resultado
+    n1 = n2;            //n1 passa a ter o valor de n2
+    n2 = n3;            //n2 vai receber o valor de n3
+}
---
+++ modulo01/semana01/Fruits/Fruits/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = 15;         //Já estava definido pelo exercício  tamanho do vetor, neste caso, 15.
+
+Console.Write("Escreva o nome de quinze frutas. \n\n");           //Solicitando ao usuário que escreva as frutas.
+
+string[] fruits = new string[n];            //Criando o vetor com o tipo string.
+
+for (int i = 0; i < n; i++)         //Enquanto a variavel 'i' for menor que a variavel 'n', as informações digitadas pelo usuario serao gravadas de caixa em caixa no vetor.
+{
+    fruits[i] = Console.ReadLine();             //Entrada de dados, até preencher o tamanho do vetor.
+}
+;
+
+Array.Sort(fruits);             //Colocando em ordem alfabetica
+Console.Write("\nFrutas em ordem alfabética\n\n");          //Espaço entre informações digitadas pelo usuário e as mesmas em ordem alfabética.
+foreach (string i in fruits) Console.WriteLine(i);          // Saida de dados em ordem alfabética.
