f0fb5bef538391e973378a57efa912573acdd166
Djamille Vieira <djamille@hotmail.com>
2023-04-04 15:14:31 -0300

    Semana10 - Criacao Projeto School

---
+++ M01S10/Modelos/MovieContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace M01S10.Modelos
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options) : base(options) 
+        { 
+        }
+
+        DbSet<Movie> Movies { get; set; } = null;
+    }
+}
