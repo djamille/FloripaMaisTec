ccca6607211875f70ee505a9173c06075ea934e3
Djamille Vieira <djamille@hotmail.com>
2023-05-22 10:15:20 -0300

    inclusao

---
+++ modulo02/semana03/ex01.js
@@ -0,0 +1,33 @@
+//Exercicio 01 semana 03 modulo 02
+
+//String - sequência de caracteres alfanuméricos envolvidos por aspas
+var nome = "Djamille Vieira";
+
+
+//Number = representação números inteiros e/ou flutuantes
+var idade = 38;
+var altura = 1.70;
+
+
+//Boolean - Representa um valor lógico verdadeiro (true) ou falso (false)
+var temFilhos = true;
+var solteira = false;
+
+
+//Object = Representa objetos, armazenando valores
+var pessoa = {
+    nome : "Djamille",
+    idade : 38,
+    cidade : "Florianópolis"
+};
+
+
+//Array - lista de objetos
+var cores = ["azul", "rosa", "amarelo", "branco"];
+
+var estados = ["Pará", "Acre", "São Paulo", "Santa Catarina"];
+
+
+//Null - ausencia intencional de valor
+var redesSociais = null;
+
