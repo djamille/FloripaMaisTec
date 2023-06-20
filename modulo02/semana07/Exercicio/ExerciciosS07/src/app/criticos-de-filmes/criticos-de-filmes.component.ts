5c3578c9cdaee2e9d683752aae3e3c26c61d2af7
Djamille Vieira <djamille@hotmail.com>
2023-06-20 16:50:26 -0300

    exercicios 04 e 05 da semana 07

--- modulo02/semana07/Exercicio/ExerciciosS07/src/app/criticos-de-filmes/criticos-de-filmes.component.ts
+++ modulo02/semana07/Exercicio/ExerciciosS07/src/app/criticos-de-filmes/criticos-de-filmes.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-criticos-de-filmes',
@@ -11,14 +11,17 @@ export class CriticosDeFilmesComponent {
 
   constructor() {
     this.avaliacaoForm = new FormGroup({
-      nome: new FormGroup(''),
-      email: new FormGroup(''),
-      nomeFilme: new FormGroup(''),
-      avaliacao: new FormGroup(''),
+      "nome": new FormControl('',Validators.required),
+      "email": new FormControl('',[Validators.required, Validators.email]),
+      "nomeFilme": new FormControl('',Validators.required),
+      "avaliacao": new FormControl('',Validators.required),
     });
   }
 
   onSubmit() {
-    console.log(this.avaliacaoForm.value);
+    console.log(this.avaliacaoForm.get("nome")?.value)
+    console.log(this.avaliacaoForm.get("email")?.value)
+    console.log(this.avaliacaoForm.get("nomeFilme")?.value)
+    console.log(this.avaliacaoForm.get("avaliacao")?.value)
   }
 }
